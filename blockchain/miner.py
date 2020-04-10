b3bd7fcaca6ba8e9deaad74d2ac7cc41a14167aa
Restyled.io <commits@restyled.io>
2020-04-10 18:18:01 +0000

    Restyled by black

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -53,7 +53,7 @@ def valid_proof(last_hash, proof):
     return hash_proof == guess_hash[:5]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # What node are we interacting with?
     if len(sys.argv) > 1:
         node = sys.argv[1]
@@ -68,7 +68,7 @@ if __name__ == '__main__':
     print("ID is", id)
     f.close()
 
-    if id == 'NONAME\n':
+    if id == "NONAME\n":
         print("ERROR: You must change your name in `my_id.txt`!")
         exit()
     # Run forever until interrupted
@@ -76,15 +76,14 @@ if __name__ == '__main__':
         # Get the last proof from the server
         r = requests.get(url=node + "/last_proof")
         data = r.json()
-        new_proof = proof_of_work(data.get('proof'))
+        new_proof = proof_of_work(data.get("proof"))
 
-        post_data = {"proof": new_proof,
-                     "id": id}
+        post_data = {"proof": new_proof, "id": id}
 
         r = requests.post(url=node + "/mine", json=post_data)
         data = r.json()
-        if data.get('message') == 'New Block Forged':
+        if data.get("message") == "New Block Forged":
             coins_mined += 1
             print("Total coins mined: " + str(coins_mined))
         else:
-            print(data.get('message'))
+            print(data.get("message"))
